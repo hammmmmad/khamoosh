9cc835d90efb07512072185cac350dec9cb386f4
Sarfraz Khamoosh <112798916+hammmmmad@users.noreply.github.com>
2026-09-06 09:44:39 -0700

    b

---
+++ supabase-web-config.js
@@ -0,0 +1,5 @@
+// This is a public browser configuration. Never place a service_role/secret key here.
+window.APP_ADMIN_SUPABASE_CONFIG = {
+  url: 'https://uqwcahtlnaodaeusmzmm.supabase.co',
+  publishableKey: 'sb_publishable_GF5HEOLIzABEsJxKeHaUmg_iUWd3YGl',
+};
